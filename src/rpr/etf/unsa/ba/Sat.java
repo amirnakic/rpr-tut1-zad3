c416fb501982cb575b044e9aa9db0546c5971606
amirnakic <anakic1@etf.unsa.ba>
2018-10-21 16:41:33 +0200

    Promjena modifikatora privatnosti metoda u klasi Sat.java.

--- src/rpr/etf/unsa/ba/Sat.java
+++ src/rpr/etf/unsa/ba/Sat.java
@@ -2,27 +2,27 @@ package rpr.etf.unsa.ba;
 
 public class Sat {
 
-    int sati,minute,sekunde;
+    private int sati,minute,sekunde;
     Sat(int sati, int minute, int sekunde) { Postavi(sati,minute,sekunde); }
-    void Postavi(int sati, int minute, int sekunde) { this.sati=sati; this.minute=minute; this.sekunde=sekunde; }
-    void Sljedeci() {
+    public void Postavi(int sati, int minute, int sekunde) { this.sati=sati; this.minute=minute; this.sekunde=sekunde; }
+    public void Sljedeci() {
         sekunde++;
         if (sekunde==60) { sekunde=0; minute++; }
         if (minute==60) { minute=0; sati++; }
         if (sati==24) sati=0;
     }
-    void Prethodni() {
+    public void Prethodni() {
         sekunde--;
         if (sekunde==-1) { sekunde=59; minute--; }
         if (minute==-1) { minute=59; sati--; }
         if (sati==-1) sati=23;
     }
-    void PomjeriZa(int pomak) {
+    public void PomjeriZa(int pomak) {
         if (pomak>0) for (int i = 0; i<pomak; i++) Sljedeci();
         else for (int i=0; i<-pomak; i++) Prethodni();
     }
-    int DajSate() { return sati; }
-    int DajMinute() { return minute; }
-    int DajSekunde() { return sekunde; }
-    void Ispisi() { System.out.printf("%d:%d:%d\n", this.DajSate(), this.DajMinute(), this.DajSekunde()); }
+    public int DajSate() { return sati; }
+    public int DajMinute() { return minute; }
+    public int DajSekunde() { return sekunde; }
+    public void Ispisi() { System.out.printf("%d:%d:%d\n", this.DajSate(), this.DajMinute(), this.DajSekunde()); }
 };
